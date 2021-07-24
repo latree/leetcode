fb6405f0d47f7752c69a1b3fa8a32a289cb4079e
latree88 <latree88@gmail.com>
2021-07-24 11:53:49 -0700

    Dot Product of Two Sparse Vectors

---
+++ Array/dot_product_two_sparse_vector.py
@@ -0,0 +1,18 @@
+from typing import List
+
+class SparseVector:
+    # solution 1:
+    # def __init__(self, nums: List[int]):
+    #     self.vector = nums.copy()
+
+    # # Return the dotProduct of two sparse vectors
+    # def dotProduct(self, vec: 'SparseVector') -> int:
+    #     return sum(i[0] * i[1] for i in zip(self.vector, vec.vector))
+
+    # solution 2:
+    
+
+# Your SparseVector object will be instantiated and called as such:
+# v1 = SparseVector(nums1)
+# v2 = SparseVector(nums2)
+# ans = v1.dotProduct(v2)
