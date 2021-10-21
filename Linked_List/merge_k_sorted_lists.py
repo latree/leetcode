f19f22a5cba673699b8192cb06f5f3806f815254
latree88 <latree88@gmail.com>
2021-10-20 21:17:18 -0700

    nice to have

--- Linked_List/merge_k_sorted_lists.py
+++ Linked_List/merge_k_sorted_lists.py
@@ -4,35 +4,72 @@ import heapq
 
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        # # 一定要审题给的lists 其实是[head, head, head]， 不是[[ListNode, ListNode], [ListNode,ListNode]]
+        # if not lists:
+        #     return
+        # m = len(lists)
+        # # 其实这道题本身并不难，难的地方在于定义heap_list里面每一个item的是什么
+        # # every element should be (c_node.val, r, c_node)
+        # heap_list = []
+        # dummy_head = ListNode(-1)
+        # tail = dummy_head
+        # # key: r, val: c_node
+        # ptr_map = {}
+        # for i in range(m):
+        #     if lists[i]:
+        #         cur_node = lists[i]
+        #         ptr_map[i] = cur_node
+        #         heapq.heappush(heap_list, (cur_node.val, i, cur_node))
+                
+        # while ptr_map:
+        #     val, r, node = heapq.heappop(heap_list)
+        #     tail.next = node
+        #     tail = tail.next
+            
+        #     n_node = node.next
+        #     if not n_node:
+        #         del ptr_map[r]
+        #     else:
+        #         n_item = (n_node.val, r, n_node)
+        #         heapq.heappush(heap_list, n_item)
+            
+        # return dummy_head.next
+            
+
         # 一定要审题给的lists 其实是[head, head, head]， 不是[[ListNode, ListNode], [ListNode,ListNode]]
+        # 其实这道题本身并不难，难的地方在于定义heap_list里面每一个item的是什么
         if not lists:
             return
-        m = len(lists)
-        # 其实这道题本身并不难，难的地方在于定义heap_list里面每一个item的是什么
-        # every element should be (c_node.val, r, c_node)
-        heap_list = []
-        dummy_head = ListNode(-1)
-        tail = dummy_head
-        # key: r, val: c_node
-        ptr_map = {}
-        for i in range(m):
-            if lists[i]:
-                cur_node = lists[i]
-                ptr_map[i] = cur_node
-                heapq.heappush(heap_list, (cur_node.val, i, cur_node))
-                
-        while ptr_map:
-            val, r, node = heapq.heappop(heap_list)
-            tail.next = node
-            tail = tail.next
-            
-            n_node = node.next
-            if not n_node:
-                del ptr_map[r]
-            else:
-                n_item = (n_node.val, r, n_node)
-                heapq.heappush(heap_list, n_item)
-            
-        return dummy_head.next
-            
         
+        m = len(lists)
+        
+        dummy = ListNode(-99999)
+        cur = dummy
+        ptrs = {}
+
+        heap_list = []
+        for i in range(m):
+            if not lists[i]:
+                continue
+            ptrs[i] = lists[i]
+            # ************* important **********************
+            # heap compare 的key 默认的是tuple 里面的第一个元素。而且heap compare的key 是无法customize的
+            heapq.heappush(heap_list, (lists[i].val, i, lists[i]))
+        
+        while ptrs:
+            value, row, cur_node = heapq.heappop(heap_list)
+            # update ptrs next ptr in this current linkedd list
+            ptrs[row] = cur_node.next
+            if not cur_node.next:
+                del ptrs[row]
+            else:
+                # add new element into priority queue
+                next_node = ptrs[row]
+                heapq.heappush(heap_list, (next_node.val, row, next_node))
+            
+            # create Node and insert to final list
+            temp = ListNode(value)
+            cur.next = temp
+            cur = cur.next
+        
+        return dummy.next
