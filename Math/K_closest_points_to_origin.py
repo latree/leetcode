42975815520075c0cfea4f50f9682d5505c3cce3
latree88 <latree88@gmail.com>
2021-10-20 21:54:23 -0700

    another question

--- Math/K_closest_points_to_origin.py
+++ Math/K_closest_points_to_origin.py
@@ -22,13 +22,29 @@ class kClosest:
         #     res += points_map[distance]
         # return res
 
-        # solution 2:
-        heap = []
-        for x, y in points:
-            distance = -(x**2 + y**2)
-            if len(heap) == k:
-                heapq.heappushpop(heap, (distance, x, y))
-            else:
-                heapq.heappush(heap, (distance, x, y))
+        # # solution 2:
+        # heap = []
+        # for x, y in points:
+        #     distance = -(x**2 + y**2)
+        #     if len(heap) == k:
+        #         heapq.heappushpop(heap, (distance, x, y))
+        #     else:
+        #         heapq.heappush(heap, (distance, x, y))
         
-        return [[x,y] for (distance, x, y) in heap]
+        # return [[x,y] for (distance, x, y) in heap]
+
+
+
+        # 第二遍
+        m = len(points)
+        heap_list = []
+        res = []
+        for i in range(m):
+            r, c = points[i]
+            dist = r**2 + c**2
+            heapq.heappush(heap_list, (dist, r, c))
+        
+        temp = list(heapq.nsmallest(k, heap_list))
+        for i in range(len(temp)):
+            res.append([temp[i][1], temp[i][2]])
+        return res
