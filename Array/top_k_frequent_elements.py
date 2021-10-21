2502cb47431ca20664941cdb9152a84d32f92d57
latree88 <latree88@gmail.com>
2021-10-20 22:05:42 -0700

    speed!

--- Array/top_k_frequent_elements.py
+++ Array/top_k_frequent_elements.py
@@ -3,11 +3,24 @@ import heapq
 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        count_map = {}
+        # count_map = {}
+        # for num in nums:
+        #     count = count_map.get(num, [num, 0])[1] + 1
+        #     count_map[num] = [num, count]
+        
+        # res = heapq.nlargest(k, count_map.values(), key=lambda e:e[1])
+        # return [item[0] for item in res]
+        
+# 第二遍
+        count = {}
+    
         for num in nums:
-            count = count_map.get(num, [num, 0])[1] + 1
-            count_map[num] = [num, count]
+            count[num] = count.get(num, 0) + 1
         
-        res = heapq.nlargest(k, count_map.values(), key=lambda e:e[1])
-        return [item[0] for item in res]
+        heap_list = []
         
+        for num, freq in count.items():
+            heapq.heappush(heap_list, (freq, num))
+        
+        res = list(heapq.nlargest(k, heap_list))
+        return [ele[1] for ele in res]
