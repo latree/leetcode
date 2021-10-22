062872ca1c58fb721f79266138051547e6d715fb
latree88 <latree88@gmail.com>
2021-10-21 23:10:28 -0700

    too tired

--- Array/subarray_sum_equals_k.py
+++ Array/subarray_sum_equals_k.py
@@ -21,24 +21,46 @@ class subarraySum:
         #             res += 1
         # return res
     
-        # solution 2: hashmap
-        # Time: O(n)
-        # Space: O(n)
-        # hash_map key：当前sum的总值。value是这个sum总值出现的次数
-        # 为什么要用sum - k？：如果sum - k 出现在hash_map中就说明之前的sum 和当前的sum相差k，那么
-        # 就是从之前的index 到现在index的sub window 之和就是k。所以就找到了sub array
-        hash_map = {}
-        hash_map[0] = 1
+        # # solution 2: hashmap
+        # # Time: O(n)
+        # # Space: O(n)
+        # # hash_map key：当前sum的总值。value是这个sum总值出现的次数
+        # # 为什么要用sum - k？：如果sum - k 出现在hash_map中就说明之前的sum 和当前的sum相差k，那么
+        # # 就是从之前的index 到现在index的sub window 之和就是k。所以就找到了sub array
+        # hash_map = {}
+        # hash_map[0] = 1
+        # cur_sum = 0
+        # count = 0
+        # for i in range(len(nums)):
+        #     cur_sum += nums[i]
+        #     cur = cur_sum - k
+        #     if cur in hash_map:
+        #         count += hash_map.get(cur)
+        #     hash_map[cur_sum] = hash_map.get(cur_sum, 0) + 1
+        # return count
+
+# 第二遍
+        # 这里用到了形成一个累加的array，在不少题目当中累加的array 都是很有用的
+        # 尤其是在sum的题型里面。
+        sum_map = {}
         cur_sum = 0
+        sum_map[0] = 1
         count = 0
         for i in range(len(nums)):
             cur_sum += nums[i]
-            cur = cur_sum - k
-            if cur in hash_map:
-                count += hash_map.get(cur)
-            hash_map[cur_sum] = hash_map.get(cur_sum, 0) + 1
+            minus_k = cur_sum - k
+            if minus_k in sum_map:
+                # 为什么count的是 sum_map[minus_k]？
+                # 因为是寻找到当前idx的sum减去k以后是不是之前的sum是等于这个值的。
+                # 如果存在那么之前那个sum的idx是i，现在的idx是j。就代表i-j的区间
+                # sum 是等于k的。
+                
+                # 另外有可能能出现当前的这个minus_k有可能有好几种情况都等于都是用到minus_k以后的idx
+                # 所以每次要加sum_map[cur_sum] 值。这样才能得到总数
+                count += sum_map[minus_k]
+            sum_map[cur_sum] = sum_map.get(cur_sum, 0) + 1
+        
         return count
-
 
     def call_function(self) -> None:
         print(self.subarraySum([1, 1, 1], 2))
--- String/group_shifted_strings.py
+++ String/group_shifted_strings.py
@@ -2,22 +2,51 @@ from typing import List
 
 class Solution:
     def groupStrings(self, strings: List[str]) -> List[List[str]]:
-        # 有两个细节要注意：
-        # 第一个是: ord_diff += (tmp_diff % 26, ) 这样的表达方式可以实现往tuple 里加东西
-        # 第二个是：要注意审题，对于每一个单独的字符向上加几个diff 而不是作为一个string 整体shift
-        res_dict = {}
+        # # 有两个细节要注意：
+        # # 第一个是: ord_diff += (tmp_diff % 26, ) 这样的表达方式可以实现往tuple 里加东西
+        # # 第二个是：要注意审题，对于每一个单独的字符向上加几个diff 而不是作为一个string 整体shift
+        # res_dict = {}
 
-        for item in strings:
-            ord_diff = ()
+        # for item in strings:
+        #     ord_diff = ()
 
-            for i in range(1, len(item)):
-                tmp_diff = 26 + ord(item[i]) - ord(item[i - 1])
-                ord_diff += (tmp_diff % 26, )
+        #     for i in range(1, len(item)):
+        #         tmp_diff = 26 + ord(item[i]) - ord(item[i - 1])
+        #         ord_diff += (tmp_diff % 26, )
                 
-            res_dict[ord_diff] = res_dict.get(ord_diff, []) + [item]
+        #     res_dict[ord_diff] = res_dict.get(ord_diff, []) + [item]
 
-        return [value for value in res_dict.values()]
+        # return [value for value in res_dict.values()]
 
+
+# 第二遍：
+#         这种方法比较容易好理解
+#         每个word都有一个base。先用word里第一个ch 找出与‘a’的diff
+#         然后用这个diff 计算这个word的base
+#         之后只要group base 一起的就好了
+        def convert_to_base(word):
+            diff = ord(word[0]) - ord('a')
+            
+            base = ''
+            for i in range(len(word)):
+                cur_ch_ord = ord(word[i]) - diff
+                if cur_ch_ord < ord('a'):
+                    # 唯一的特殊情况就是这个得出的unicode 小于‘a’的unicode，那么我们就要加26 
+                    # 比如az 和ba， ba 的a 减1 就是负的了
+                    cur_ch_ord += 26
+                base += chr(cur_ch_ord)    
+            return base
+        
+        base_map = {}
+        
+        for i in range(len(strings)):
+            word = strings[i]
+            cur_base = convert_to_base(word)
+            if cur_base not in base_map:
+                base_map[cur_base] = []
+            base_map[cur_base].append(word)
+        
+        return [v for v in base_map.values()]
     
     def call_function(self):
         self.groupStrings(["abc","bcd","acef","xyz","az","ba","a","z"])
