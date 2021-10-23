b06ef2069ce55be2eaa0df79c13f07be120b9e65
latree88 <latree88@gmail.com>
2021-10-22 20:36:47 -0700

    start!

--- Math/random_pick_index.py
+++ Math/random_pick_index.py
@@ -2,21 +2,28 @@ from typing import List
 import random 
 
 class Solution:
+    # def __init__(self, nums: List[int]):
+    #     self.nums_map = {}
+    #     for i, num in enumerate(nums):
+    #         self.nums_map[num] = self.nums_map.get(num, []) + [i]
 
-    def __init__(self, nums: List[int]):
-        self.nums_map = {}
-        for i, num in enumerate(nums):
-            self.nums_map[num] = self.nums_map.get(num, []) + [i]
-
-    def pick(self, target: int) -> int:
-        n = len(self.nums_map[target])
-        if n > 1:
-            idx = random.randint(0, n - 1)
-            return self.nums_map[target][idx]
-        else:
-            return self.nums_map[target][0]
+    # def pick(self, target: int) -> int:
+    #     n = len(self.nums_map[target])
+    #     if n > 1:
+    #         idx = random.randint(0, n - 1)
+    #         return self.nums_map[target][idx]
+    #     else:
+    #         return self.nums_map[target][0]
  
 
-# Your Solution object will be instantiated and called as such:
-# obj = Solution(nums)
-# param_1 = obj.pick(target)
+# 第二遍：
+    def __init__(self, nums: List[int]):
+        self.idx_map = {}
+        for i in range(len(nums)):
+            if nums[i] not in self.idx_map:
+                self.idx_map[nums[i]] = []
+            self.idx_map[nums[i]].append(i)
+
+    def pick(self, target: int) -> int:
+        target_idx = random.randint(0, len(self.idx_map[target]) - 1)
+        return self.idx_map[target][target_idx]
