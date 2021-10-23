394e6e00af7f093bf561f1e2af94e58ba4fbb9bf
latree88 <latree88@gmail.com>
2021-10-22 21:22:21 -0700

    easy one

--- String/palindrome_permutation.py
+++ String/palindrome_permutation.py
@@ -1,14 +1,27 @@
 class Solution:
     def canPermutePalindrome(self, s: str) -> bool:
+        # char_map = {}
+        # for ch in s:
+        #     char_map[ch] = char_map.get(ch, 0) + 1
+        
+        # count = 0
+        # for k in char_map:
+        #     count += char_map[k] % 2
+        
+        # if count == 1 or count == 0:
+        #     return True
+        # return False
+    
+
+    # 第二遍
         char_map = {}
+        odd_count = 0
         for ch in s:
             char_map[ch] = char_map.get(ch, 0) + 1
         
-        count = 0
-        for k in char_map:
-            count += char_map[k] % 2
+        for ch in char_map:
+            if char_map[ch] % 2 != 0:
+                odd_count += 1
         
-        if count == 1 or count == 0:
-            return True
-        return False
-    
+        return odd_count <= 1
+        
