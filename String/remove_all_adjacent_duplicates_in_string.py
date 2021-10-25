e2099040223f5e0de96314d1a04734fef4e1488b
latree88 <latree88@gmail.com>
2021-10-24 19:23:12 -0700

    nice

--- String/remove_all_adjacent_duplicates_in_string.py
+++ String/remove_all_adjacent_duplicates_in_string.py
@@ -1,21 +1,34 @@
 class Solution:
     def removeDuplicates(self, s: str) -> str:
-        # 要充分理解一下adjacent 这个词，只能是看到第一个重复的时候remove，不是看到所有重复的以后再remove
-        stack = []
+#         # 要充分理解一下adjacent 这个词，只能是看到第一个重复的时候remove，不是看到所有重复的以后再remove
+#         stack = []
         
-        for ch in s:
-            if stack and ch == stack[-1]:
-                stack.pop()
+#         for ch in s:
+#             if stack and ch == stack[-1]:
+#                 stack.pop()
+#             else:
+#                 stack.append(ch)
+#         return "".join(stack)
+
+
+# # Input: s = "abbaca"
+# # Output: "ca"
+
+# # a b  b  b  a c a 
+# #              i
+
+
+# # stack = [c]
+
+        # 第二遍
+        dup_stack = []
+        for i in range(len(s)):
+            if dup_stack:
+                if s[i] == dup_stack[-1]:
+                    dup_stack.pop()
+                else:
+                    dup_stack.append(s[i])
             else:
-                stack.append(ch)
-        return "".join(stack)
-
-
-# Input: s = "abbaca"
-# Output: "ca"
-
-# a b  b  b  a c a 
-#              i
-
-
-# stack = [c]
+                dup_stack.append(s[i])
+        
+        return "".join(dup_stack)
