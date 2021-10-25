31a43afe80811e9de4f6e4b2a8f6d389d8cbab65
latree88 <latree88@gmail.com>
2021-10-24 20:33:18 -0700

    nice to have

--- Array/exclusive_time_of_functions.py
+++ Array/exclusive_time_of_functions.py
@@ -2,26 +2,52 @@ from typing import List
 
 class Solution:
     def exclusiveTime(self, n: int, logs: List[str]) -> List[int]:    
-        stack = []
-        pre_time = 0
-        res = [0] * n
-        for log in logs:
-            func, type, timestamp = log.split(":")
-            func, timestamp = int(func), int(timestamp)
+        # stack = []
+        # pre_time = 0
+        # res = [0] * n
+        # for log in logs:
+        #     func, type, timestamp = log.split(":")
+        #     func, timestamp = int(func), int(timestamp)
             
-            if type == 'start':
+        #     if type == 'start':
+        #         if stack:
+        #             res[stack[-1]] += timestamp - pre_time
+        #         stack.append(func)
+        #         pre_time = timestamp
+        #     else:
+        #         # 这两部很关键 pre_time = timestamp + 1是我没有想到的。
+        #         # 你可以想象成每一个时间都是有准确节点的。
+        #         # 在5结束的时候其实就是相当于在6开始的时候结束，所以要在previous time的基础上加1
+        #         res[stack.pop()] += timestamp - pre_time + 1
+        #         pre_time = timestamp + 1
+            
+        # return res
+    
+
+# 第二遍
+        # stop of stack is the current fuction id
+        # pre_time 记录了当前stack上的最近的timestamp 时间。每新读一个log的时间都需要pre_time来计算当前stack上的function 跑了多久了
+        stack = []
+        res = [0] * n
+        pre_time = 0
+        for i in range(len(logs)):
+            cur_log = logs[i].split(":")
+            f_id, state, timestamp = int(cur_log[0]), cur_log[1], int(cur_log[2])
+            # 这里比较容易混淆。不管当前的log 和top of stack是不是同一个function。都不重要。
+            # 重要的是如果state是start 那么我们就要结算一下当前的top of stack的function run 了多久。
+            # 如果state是end 那么正好是当前的这个top of stack的function从开始run到结束了。那也要结算一些这个function run了多久，然后把这个function pop 出stack
+            if state == "start":
                 if stack:
                     res[stack[-1]] += timestamp - pre_time
-                stack.append(func)
+                stack.append(f_id)
                 pre_time = timestamp
             else:
-                # 这两部很关键 pre_time = timestamp + 1是我没有想到的。
-                # 你可以想象成每一个时间都是有准确节点的。
-                # 在5结束的时候其实就是相当于在6开始的时候结束，所以要在previous time的基础上加1
-                res[stack.pop()] += timestamp - pre_time + 1
+                res[stack[-1]] += timestamp - pre_time + 1
+                stack.pop()
                 pre_time = timestamp + 1
-            
+        
         return res
-    
+
+
     def call_function(self):
         self.exclusiveTime(2, ["0:start:0","1:start:2","1:end:5","0:end:6"])
--- String/remove_all_adjacent_duplicates_in_string_II.py
+++ String/remove_all_adjacent_duplicates_in_string_II.py
@@ -1,18 +1,42 @@
 class Solution:
     def removeDuplicates(self, s: str, k: int) -> str:
-        # [[a, 1], [b, 1]]
+        # # [[a, 1], [b, 1]]
+        # stack = []
+        # for i in range(len(s)):
+        #     if not stack:
+        #         stack.append([s[i], 1])
+        #     elif stack[-1][0] != s[i]:
+        #         stack.append([s[i], 1])
+        #     elif stack[-1][0] == s[i]:
+        #         if stack[-1][1] == k - 1:
+        #             for _ in range(k - 1):
+        #                 stack.pop()
+        #         else:
+        #             count = stack[-1][1]
+        #             stack.append([s[i], count + 1])
+        
+        # return "".join([i for i, v in stack])
+
+
+# 第二遍
+        # 这道题是运用stack 来记录每一个字母以及当前的count
         stack = []
+        
         for i in range(len(s)):
             if not stack:
-                stack.append([s[i], 1])
+                stack.append((s[i], 1))
             elif stack[-1][0] != s[i]:
-                stack.append([s[i], 1])
+                stack.append((s[i], 1))
             elif stack[-1][0] == s[i]:
                 if stack[-1][1] == k - 1:
                     for _ in range(k - 1):
                         stack.pop()
                 else:
-                    count = stack[-1][1]
-                    stack.append([s[i], count + 1])
+                    stack.append((s[i], stack[-1][1] + 1))
+
+        res = ""
+        for i in range(len(stack)):
+            ch, count = stack[i]
+            res += ch
         
-        return "".join([i for i, v in stack])
+        return res
