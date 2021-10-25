532d04b4986c1057f763e4b08bf7c093dd15c75b
latree88 <latree88@gmail.com>
2021-10-24 19:05:59 -0700

    can not concentrate

--- Array/buildings_with_ocean_view.py
+++ Array/buildings_with_ocean_view.py
@@ -2,15 +2,27 @@ from typing import List
 
 class findBuildings:
     def findBuildings(self, heights: List[int]) -> List[int]:
-        if len(heights) == 1:
-            return [0]
-        slow, fast = len(heights) - 1, len(heights) - 2 
-        building_idx = [slow]
-        while fast >=0:
-            if heights[fast] > heights[slow]:
-                building_idx.append(fast)
-                slow = fast
+        # if len(heights) == 1:
+        #     return [0]
+        # slow, fast = len(heights) - 1, len(heights) - 2 
+        # building_idx = [slow]
+        # while fast >=0:
+        #     if heights[fast] > heights[slow]:
+        #         building_idx.append(fast)
+        #         slow = fast
                 
-            fast -= 1
+        #     fast -= 1
 
-        return building_idx[::-1]
+        # return building_idx[::-1]
+
+
+        # 第二遍
+        # 这个其实就是用到monotonic stack 的概念。只往stack append 比当前最大值还要再大的值
+        max_height = 0
+        res = []
+        for i in range(len(heights) -  1, -1, -1):
+            if heights[i] > max_height:
+                res.append(i)
+                max_height = heights[i]
+            
+        return res[::-1]
