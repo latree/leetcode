b0e4d72ba3f7a8475124318d240d7e742fa39ea7
latree88 <latree88@gmail.com>
2021-10-25 23:11:05 -0700

    done

--- Array/range_sum_query_2d-immutable.py
+++ Array/range_sum_query_2d-immutable.py
@@ -1,21 +1,39 @@
 from typing import List
 
 class NumMatrix:
-    # 大格子减小格子
+    # # 大格子减小格子
+    # def __init__(self, matrix: List[List[int]]):
+    #     if not matrix or not matrix[0]:
+    #         return
+    #     N = len(matrix) + 1
+    #     M = len(matrix[0]) + 1
+    #     self.dp = [ [ 0 for i in range(M) ] for j in range(N) ]
+        
+    #     for r in range(len(matrix)):
+    #         for c in range(len(matrix[0])):
+    #             self.dp[r + 1][c + 1] = matrix[r][c] + self.dp[r + 1][c] + self.dp[r][c + 1] - self.dp[r][c]
+        
+    # def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
+    #     return self.dp[row2 + 1][col2 + 1] - self.dp[row1][col2 + 1] - self.dp[row2 + 1][col1] + self.dp[row1][col1]
+
+
+# 第二遍
     def __init__(self, matrix: List[List[int]]):
         if not matrix or not matrix[0]:
             return
         N = len(matrix) + 1
         M = len(matrix[0]) + 1
-        self.dp = [ [ 0 for i in range(M) ] for j in range(N) ]
+        self.prefix_sum = [[0 for _ in range(M)] for _ in range(N)]
         
-        for r in range(len(matrix)):
-            for c in range(len(matrix[0])):
-                self.dp[r + 1][c + 1] = matrix[r][c] + self.dp[r + 1][c] + self.dp[r][c + 1] - self.dp[r][c]
+        for i in range(N - 1):
+            for j in range(M - 1):
+                # 这里有一出会让人非常容易混淆。就是为什么加的是matrix[i][j]而不是matrix[i + 1][j + 1]是因为prefix_sum多加了一个row 和一个col。所以
+                # 这里的matrix[i][j] 其实就是相当于prefix_sum[i + 1][j + 1]
+                self.prefix_sum[i + 1][j + 1] = matrix[i][j] + self.prefix_sum[i][j + 1] + self.prefix_sum[i + 1][j] - self.prefix_sum[i][j]
+        
         
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
-        return self.dp[row2 + 1][col2 + 1] - self.dp[row1][col2 + 1] - self.dp[row2 + 1][col1] + self.dp[row1][col1]
-
+        return self.prefix_sum[row2 + 1][col2 + 1] - self.prefix_sum[row1][col2 + 1] - self.prefix_sum[row2 + 1][col1] + self.prefix_sum[row1][col1]
 
 # Your NumMatrix object will be instantiated and called as such:
 # obj = NumMatrix(matrix)
