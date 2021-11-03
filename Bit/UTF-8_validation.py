432fce7514dd95d80b01513607503e4b3d0456dd
latree88 <latree88@gmail.com>
2021-11-02 21:29:56 -0700

    too tired

--- Bit/UTF-8_validation.py
+++ Bit/UTF-8_validation.py
@@ -2,29 +2,61 @@ from typing import List
 
 class Solution:
     def validUtf8(self, data: List[int]) -> bool:
+        # mask1 = 1 << 7
+        # mask2 = 1 << 6
+        # n_bytes = 0
+        
+        # for num in data:
+        #     mask = 1 << 7
+            
+        #     if n_bytes ==0:
+        #         # So, we have taken a mask = 1 << 7 which is basically 10000000. We will make use of this mask and logically 
+        #         # and it with the number to see if the bit at a particular position is set or not. 
+        #         # We do this iteratively to check how many bits are set starting from the most significant bit 
+        #         # (Remember, the integer might be too large but we should only process the 8 least significant bits of data.)
+        #         while num & mask:
+        #             n_bytes += 1
+        #             mask = mask >> 1
+                
+        #         if n_bytes == 0:
+        #             continue
+        #         if n_bytes == 1i or n_bytes > 4:
+        #             return False
+                
+        #     else:
+        #         # The below code will simple use the mask1 to check if the most significant bit is set to 1 
+        #         # and the second most significant bit is set to 0. if this is not a case, then we return False.
+        #         if not (num & mask1 and not (num & mask2)):
+        #             return False
+            
+        #     n_bytes -= 1
+        # return n_bytes == 0
+
+        # 原理：
+        # 1. 用mask 1000000, 01000000 来做与还看看第一位和第二位是1 还是0 
+        # 2. 先来count n_bytes有几个，然后每过一个num 那么就n_bytes - 1。 最后看看count的n_bytes 和num的数量符不符合。不符合就是false
+        # 3. n_bytes > 4 肯定是错误的，n_bytes==1 说明左边第一位是1 那么也是false
+        # 4. 剩余的bytes 的前两位不是10 那就是false。
         mask1 = 1 << 7
         mask2 = 1 << 6
         n_bytes = 0
-        
+
         for num in data:
             mask = 1 << 7
             
-            if n_bytes ==0:
-                # So, we have taken a mask = 1 << 7 which is basically 10000000. We will make use of this mask and logically and it with the number to see if the bit at a particular position is set of not. We do this iteratively to check how many bits are set starting from the most significant bit (Remember, the integer might be too large but we should only process the 8 least significant bits of data.)
+            if n_bytes == 0:    
                 while num & mask:
                     n_bytes += 1
                     mask = mask >> 1
-                
+
                 if n_bytes == 0:
                     continue
                 if n_bytes == 1 or n_bytes > 4:
                     return False
-                
             else:
-                # The below code will simple use the mask1 to check if the most significant bit is set to 1 and the second most significant bit is set to 0. if this is not a case, then we return False.
-                if not (num & mask1 and not (num & mask2)):
+                if not (num & mask1 and not(num & mask2)):
                     return False
-            
+        
             n_bytes -= 1
+        
         return n_bytes == 0
-                
--- Others/read_n_characters_given_read4_ii-call_multiple_times.py
+++ Others/read_n_characters_given_read4_ii-call_multiple_times.py
@@ -1,38 +1,65 @@
 from typing import List
-
+from collections import deque
 # # The read4 API is already defined for you.
 # # def read4(buf4: List[str]) -> int:
 
 class Solution:
-    def __init__(self):
-        self.buff_size = 4
-        self.buffer = [None for i in range(self.buff_size)]
-        self.char_left_over = 0
-        self.left_over_ptr = 0
+    # def __init__(self):
+    #     self.buff_size = 4
+    #     self.buffer = [None for i in range(self.buff_size)]
+    #     self.char_left_over = 0
+    #     self.left_over_ptr = 0
         
-    def read(self, buf: List[str], n: int) -> int:
-        char_reads = 0
-        cur_read = 0
-        if self.char_left_over:
-            to_read = min(n, self.char_left_over)
-            buf[0:to_read] = self.buffer[-self.char_left_over - self.left_over_ptr:]
-            char_reads += to_read
-            self.char_left_over -= to_read
+    # def read(self, buf: List[str], n: int) -> int:
+    #     char_reads = 0
+    #     cur_read = 0
+    #     if self.char_left_over:
+    #         to_read = min(n, self.char_left_over)
+    #         buf[0:to_read] = self.buffer[-self.char_left_over - self.left_over_ptr:]
+    #         char_reads += to_read
+    #         self.char_left_over -= to_read
             
             
-        while char_reads < n:
-            cur_read = read4(self.buffer)
-            if not cur_read:
-                break
+    #     while char_reads < n:
+    #         cur_read = read4(self.buffer)
+    #         if not cur_read:
+    #             break
             
-            buf[char_reads:char_reads + cur_read] = self.buffer[:cur_read]
+    #         buf[char_reads:char_reads + cur_read] = self.buffer[:cur_read]
 
-            char_reads += cur_read
+    #         char_reads += cur_read
         
-        res = min(char_reads, n)
+    #     res = min(char_reads, n)
         
-        if not self.char_left_over:
-            self.char_left_over = char_reads - n if char_reads > n else 0
-            self.left_over_ptr = self.buff_size - cur_read if cur_read < 4 else 0
+    #     if not self.char_left_over:
+    #         self.char_left_over = char_reads - n if char_reads > n else 0
+    #         self.left_over_ptr = self.buff_size - cur_read if cur_read < 4 else 0
         
-        return res
+    #     return res
+
+
+# 第二遍
+    def __init__(self):
+        self.buffer = deque()
+    
+    def read(self, buf: List[str], n: int) -> int:        
+        # 这道题没有那么复杂
+        # 原理就是每次从read4里面读完以后把所有值都append 到buffer 里面
+        # 然后每一次只复制给buf的次数是len(self.buffer), n - idx。两个的最小值
+        # 这样就能巧妙的保证里每一次先用掉buffer里面的所有值，然后不够再进入下一个循环，够了
+        # 就直接退出循环
+        idx = 0
+        while True:
+            buf4 = [""]*4
+
+            l = read4(buf4)
+            self.buffer.extend(buf4)
+            # 这两步很精髓
+            cur_size = min(len(self.buffer), n - idx)
+            for _ in range(cur_size):
+                buf[idx] = self.buffer.popleft()
+                idx += 1
+            
+            if cur_size == 0:
+                break
+        return idx
