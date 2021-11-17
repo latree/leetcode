3e82bca51de3be8d60f40cebea30763242c395a3
latree88 <latree88@gmail.com>
2021-11-16 22:35:50 -0800

    good problem

---
+++ Array/partition_to_k_equal_sum_subsets.py
@@ -0,0 +1,44 @@
+from typing import List
+
+
+class Solution:
+    def canPartitionKSubsets(self, nums: List[int], k: int) -> bool:
+        # 这道题也需要画出recursion tree
+        # 每一个node 应该是bucket，而且表示了把当前index 的nums的数字放进bucket以后的状态，
+        # 每一个node会有四个分枝，代表把当前的数字分别放进四个bucket里面
+        # 一共有len(nums) 层的tree
+        # 剪枝的过程就是要把当前大于target的bucket以及之后的树枝都减掉
+        def dfs(nums, idx, bucket, target):
+            if idx == len(nums):
+                for ele in bucket:
+                    if ele != target:
+                        return False
+                return True
+            
+            for i in range(len(bucket)):
+                
+                if bucket[i] + nums[idx] > target:
+                    continue
+                
+                bucket[i] += nums[idx]
+                if dfs(nums, idx + 1, bucket, target):
+                    return True
+                bucket[i] -= nums[idx]
+            
+            return False
+        
+        if k > len(nums):
+            return False
+        if sum(nums) % k != 0:
+            return False
+        
+        nums.sort(reverse=True)
+        
+        bucket = [0] * k
+        
+        target = sum(nums) / k
+        
+        return dfs(nums, 0, bucket, target)
+        
+
+        
