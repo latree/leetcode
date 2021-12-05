1ff993d5b2507198d065aa0a2608f6634bba81ee
latree88 <latree88@gmail.com>
2021-12-05 15:42:54 -0800

    5 more

--- Tree/range_sum_of_BST.py
+++ Tree/range_sum_of_BST.py
@@ -4,6 +4,10 @@ class Solution:
     def rangeSumBST(self, root: TreeNode, low: int, high: int) -> int:
         # Time: O(n)
         # Space: O(n)
+        # 最简单的写法就是不带if的traverse整个tree ，然后只要node.val 在range之内就加进去
+        # 但是这样的话需要遍历整个tree
+        # 优化的方法就是不去遍历那些不在range里面的node。所以要加上一些if condition去遍历tree node
+        
         if not root:
             return 0
         res = 0
