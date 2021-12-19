f14549844cb6df6722226ec14a52e2711d9fa94a
latree88 <latree88@gmail.com>
2021-12-19 11:56:42 -0800

    nice morning

--- String/verify_alien_dict.py
+++ String/verify_alien_dict.py
@@ -47,7 +47,27 @@ class AlienSorted:
 
 
 
-# 第二遍：
+# # 第二遍：
+#         order_map = {}
+#         for idx, ch in enumerate(order):
+#             order_map[ch] = idx
+        
+#         for i in range(len(words) - 1):
+#             for j in range(len(words[i])):
+#                 # 这个条件到了当前的这个j的时候，也就是说
+#                 # 第一个词的长度比第二个词的长度要长。并且之前的字母都相等。
+#                 # 那么这个时候直接return False
+#                 if j >= len(words[i + 1]):
+#                     return False
+#                 if words[i][j] != words[i + 1][j]:
+#                     if order_map[words[i][j]] > order_map[words[i + 1][j]]:
+#                         return False
+#                     # 这个条件比较容易漏掉。
+#                     # 如果当前的两个词的字母不相等，并且符合所给的顺序，那么我们就不用管之后的字母的顺序了
+#                     break
+#         return True
+
+# 第三遍
         order_map = {}
         for idx, ch in enumerate(order):
             order_map[ch] = idx
@@ -62,10 +82,13 @@ class AlienSorted:
                 if words[i][j] != words[i + 1][j]:
                     if order_map[words[i][j]] > order_map[words[i + 1][j]]:
                         return False
+                    # ******* important **************
+                    # 第三次刷的时候这个条件还是漏掉了！！！！！
                     # 这个条件比较容易漏掉。
                     # 如果当前的两个词的字母不相等，并且符合所给的顺序，那么我们就不用管之后的字母的顺序了
                     break
         return True
+
 
     def call_function(self) -> None:
         self.isAlienSorted(["apple","app"], "abcdefghijklmnopqrstuvwxyz")
