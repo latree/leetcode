f7141d45cc7d9a761d8a5444c14f986e3d89bbcb
latree88 <latree88@gmail.com>
2021-12-30 14:03:03 -0800

    good to have

--- String/valid_palindrome.py
+++ String/valid_palindrome.py
@@ -1,21 +1,40 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        i, j = 0, len(s) - 1
+        # i, j = 0, len(s) - 1
 
-        while i < j:
-            if s[i] == ' ' or not s[i].isalnum():
+        # while i < j:
+        #     if s[i] == ' ' or not s[i].isalnum():
+        #         i += 1
+        #         continue
+        #     elif s[j] == ' ' or not s[j].isalnum():
+        #         j -= 1
+        #         continue
+        #     else:
+        #         if s[i].lower() != s[j].lower():
+        #             return False
+        #     i += 1
+        #     j -= 1
+        # return True 
+
+# 第二遍
+        i, j = 0, len(s) - 1
+        while i <= j:
+            if not s[i].isalnum():
                 i += 1
+                # continue 会miss 掉
                 continue
-            elif s[j] == ' ' or not s[j].isalnum():
-                j -= 1
+            if not s[j].isalnum():
+                j = 1
+                # continue 会miss 掉
                 continue
-            else:
-                if s[i].lower() != s[j].lower():
-                    return False
+            if s[i].lower() != s[j].lower():
+                return False
+            
             i += 1
             j -= 1
+            
         return True 
-
+        
 
 
 # s = "A man, a plan, a canal: Panama"
