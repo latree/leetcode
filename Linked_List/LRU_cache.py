daf8d97821af55be0ffc24d7678064f02a92b28e
latree88 <latree88@gmail.com>
2021-12-30 15:30:31 -0800

    feel nice

--- Linked_List/LRU_cache.py
+++ Linked_List/LRU_cache.py
@@ -70,21 +70,102 @@ class LRUCache:
 # obj.put(key,value)
 
 
-
-# orderedDict version
-    def __init__(self, capacity: int):
-        self.capacity = capacity
-        self.order_dict = OrderedDict()
+# # ********** important ******************
+# # orderedDict version
+#     def __init__(self, capacity: int):
+#         self.capacity = capacity
+#         self.order_dict = OrderedDict()
         
+#     def get(self, key: int) -> int:
+#         if key not in self.order_dict:
+#             return -1
+#         self.order_dict.move_to_end(key)
+#         return self.order_dict[key]
+
+#     def put(self, key: int, value: int) -> None:
+#         if key in self.order_dict:
+#             self.order_dict.move_to_end(key)
+#         self.order_dict[key] = value
+#         if len(self.order_dict) > self.capacity:
+#             self.order_dict.popitem(last = False)
+
+
+# # 第二遍
+# class DlinkedNode:
+#     def __init__(self, key, val, prev=None, next=None):
+#         self.key = key
+#         self.val = val
+#         self.prev = prev
+#         self.next = next
+
+# class LRUCache:
+
+#     def __init__(self, capacity: int):
+#         self.cap = capacity
+#         self.node_map = {}
+#         self.head = DlinkedNode(-1, -1)
+#         self.tail = DlinkedNode(-1, -1)
+#         self.head.next = self.tail
+#         self.tail.prev = self.head
+
+#     def get(self, key: int) -> int:
+#         if key not in self.node_map:
+#             return -1
+        
+#         node = self.node_map[key]
+#         self.remove_node(node)
+#         self.add_node(node)
+#         return node.val
+        
+
+#     def put(self, key: int, value: int) -> None:
+#         if key not in self.node_map:
+#             if len(self.node_map) >= self.cap:
+#                 # ********* important ***********
+#                 # 这里非常容易出错。必须要先pop node_map里的key 然后再delete node
+#                 # 因为如果先delete node 以后那么self.tail.prev 就会改变。那么self.tail.prev.key也会改变，那么就回pop错value
+#                 self.node_map.pop(self.tail.prev.key)
+#                 self.remove_node(self.tail.prev)
+#             node = DlinkedNode(key, value)
+#             self.add_node(node)
+#             self.node_map[key] = node
+#         else:
+#             node = self.node_map[key]
+#             node.key = key
+#             node.val = value
+#             self.remove_node(node)
+#             self.add_node(node)
+
+    
+#     def add_node(self, node):
+#         node.next = self.head.next
+#         node.prev = self.head
+#         node.next.prev = node
+#         node.prev.next = node
+    
+#     def remove_node(self, node):
+#         cur_prev = node.prev
+#         cur_next = node.next
+#         cur_prev.next = cur_next
+#         cur_next.prev = cur_prev
+
+
+# 第二遍： OrderedDict
+    def __init__(self, capacity: int):
+        self.cap = capacity
+        self.cache = OrderedDict()
+
     def get(self, key: int) -> int:
-        if key not in self.order_dict:
+        if key not in self.cache:
             return -1
-        self.order_dict.move_to_end(key)
-        return self.order_dict[key]
+        self.cache.move_to_end(key)
+        return self.cache[key]    
 
     def put(self, key: int, value: int) -> None:
-        if key in self.order_dict:
-            self.order_dict.move_to_end(key)
-        self.order_dict[key] = value
-        if len(self.order_dict) > self.capacity:
-            self.order_dict.popitem(last = False)
+        # ********** important ************
+        # 简化了这里的步骤。 不管key在不在这里，都应该直接update key value ，然后move到最后
+        # 最后如果多了就pop最前面的
+        self.cache[key] = value
+        self.cache.move_to_end(key)
+        if len(self.cache) > self.cap:
+            self.cache.popitem(last=False)
