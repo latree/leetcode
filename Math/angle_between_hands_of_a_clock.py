f165d0cf945d4689cc8eb38cecbdada6ccf39e91
latree88 <latree88@gmail.com>
2021-12-31 12:13:26 -0800

    good

--- Math/angle_between_hands_of_a_clock.py
+++ Math/angle_between_hands_of_a_clock.py
@@ -12,20 +12,30 @@ class Solution:
         # return 360 - res if res > 180 else res
 
 
-        # 第二遍
-        hour_degree_unit = 360 / 12
-        mins_in_hour = 60
+        # # 第二遍
+        # hour_degree_unit = 360 / 12
+        # mins_in_hour = 60
         
-        total_degree = 360
-        half_degree = 360 / 2
+        # total_degree = 360
+        # half_degree = 360 / 2
         
-        # 这个就是公式
-        # 小时的度数就是，小时的整度数 + 在那一个小时里面分钟的度数
-        hour_degree = hour_degree_unit * hour + minutes / mins_in_hour * hour_degree_unit
+        # # 这个就是公式
+        # # 小时的度数就是，小时的整度数 + 在那一个小时里面分钟的度数
+        # hour_degree = hour_degree_unit * hour + minutes / mins_in_hour * hour_degree_unit
         
-        # 分钟的度数比较直接，就是当前分钟减去一个小时的分钟数再乘以360度
-        minutes_degree = minutes / mins_in_hour * total_degree
+        # # 分钟的度数比较直接，就是当前分钟减去一个小时的分钟数再乘以360度
+        # minutes_degree = minutes / mins_in_hour * total_degree
         
-        res = minutes_degree - hour_degree
+        # res = minutes_degree - hour_degree
         
-        return abs(res) if abs(res) < half_degree else total_degree - abs(res)
+        # return abs(res) if abs(res) < half_degree else total_degree - abs(res)
+
+
+        # 第三遍：
+        min_angle = minutes / 60 * 360
+        hour_angle = (60 / 12 * (minutes / 60) + hour * 60 / 12) / 60 * 360
+        
+        angle = hour_angle - min_angle if hour_angle > min_angle else min_angle - hour_angle
+        abs_angle = angle if angle < 180 else 360 - angle
+        
+        return abs(abs_angle)
