f04239b12dc5d6e46707e86ba0cb77ffbce24905
latree88 <latree88@gmail.com>
2022-01-06 16:40:56 -0800

    good

--- Array/3sum_closet.py
+++ Array/3sum_closet.py
@@ -3,24 +3,54 @@ import math
 
 class Solution:
     def threeSumClosest(self, nums: List[int], target: int) -> int:
+        # nums.sort()
+        # diff = math.inf
+        
+        # for i in range(len(nums)):
+        #     l, h = i + 1, len(nums) - 1
+            
+        #     while l < h:
+        #         three_sum = nums[i] + nums[l] + nums[h]
+                
+        #         if abs(target - three_sum) < abs(diff):
+        #             # 这里整数和负数代表了这个diff 离target是从左右多远的距离，还是从右有多远的距离。
+        #             # 不可以是abs，如果是abs那么在决定增加low，还是降低high的时候就会出错。
+        #             diff = target - three_sum
+        #         # 这里应该是和target 做比较，如果three_sum 小于target，那么就是挪动l
+        #         if three_sum < target:
+        #             l += 1
+        #         else:
+        #             h -= 1
+        #     if diff == 0:
+        #         break
+        # return target - diff
+
+
+# 第二遍
+        # 因为这里不是要求是连续的三个nums的元素。那么我们就用最基础的方法：每一个循环固定一个变量，然后根据计算来左右移动剩下的两个变量。这个前提必须是一个sorted list
         nums.sort()
-        diff = math.inf
+        diff = -math.inf
         
         for i in range(len(nums)):
             l, h = i + 1, len(nums) - 1
             
             while l < h:
-                three_sum = nums[i] + nums[l] + nums[h]
+                three = nums[i] + nums[l] + nums[h]
                 
-                if abs(target - three_sum) < abs(diff):
+                if abs(target - three) < abs(diff):
+                    # ****** important ********
+                    # 这里还是错了
                     # 这里整数和负数代表了这个diff 离target是从左右多远的距离，还是从右有多远的距离。
                     # 不可以是abs，如果是abs那么在决定增加low，还是降低high的时候就会出错。
-                    diff = target - three_sum
-                # 这里应该是和target 做比较，如果three_sum 小于target，那么就是挪动l
-                if three_sum < target:
+                    diff = target - three
+                
+                if three < target:
                     l += 1
+                
                 else:
                     h -= 1
+            
             if diff == 0:
                 break
+                
         return target - diff
--- Tree/all_nodes_distance_k_in_binary_tree.py
+++ Tree/all_nodes_distance_k_in_binary_tree.py
@@ -3,40 +3,40 @@ from typing import List
 
 class Solution:
     def distanceK(self, root: TreeNode, target: TreeNode, k: int) -> List[int]:
-        res = []
-        # 用return 的value 表示distance from node to target, 如果target node 在左边，那么如果要move 到node.right 那么distance就要
-        # l + 1， 右边也是同理
-        def dfs(node):
-            if not node:
-                return -1
-            elif node is target:
-                subtree_add(node, 0)
-                return 1
-            else:
-                l, r = dfs(node.left), dfs(node.right)
-                if l != -1:
-                    if l == k: res.append(node.val)
-                    subtree_add(node.right, l + 1)
-                    return l + 1
-                elif r != -1:
-                    if r == k: res.append(node.val)
-                    subtree_add(node.left, r + 1)
-                    return r + 1
-                else:
-                    return -1
+        # res = []
+        # # 用return 的value 表示distance from node to target, 如果target node 在左边，那么如果要move 到node.right 那么distance就要
+        # # l + 1， 右边也是同理
+        # def dfs(node):
+        #     if not node:
+        #         return -1
+        #     elif node is target:
+        #         subtree_add(node, 0)
+        #         return 1
+        #     else:
+        #         l, r = dfs(node.left), dfs(node.right)
+        #         if l != -1:
+        #             if l == k: res.append(node.val)
+        #             subtree_add(node.right, l + 1)
+        #             return l + 1
+        #         elif r != -1:
+        #             if r == k: res.append(node.val)
+        #             subtree_add(node.left, r + 1)
+        #             return r + 1
+        #         else:
+        #             return -1
 
-        # 找到在subtree里面所有符合dist == k的node
-        def subtree_add(node, dist):
-            if not node:
-                return 
-            elif dist == k:
-                res.append(node.val)
-            else:
-                subtree_add(node.left, dist + 1)
-                subtree_add(node.right, dist + 1)
+        # # 找到在subtree里面所有符合dist == k的node
+        # def subtree_add(node, dist):
+        #     if not node:
+        #         return 
+        #     elif dist == k:
+        #         res.append(node.val)
+        #     else:
+        #         subtree_add(node.left, dist + 1)
+        #         subtree_add(node.right, dist + 1)
         
-        dfs(root)
-        return res
+        # dfs(root)
+        # return res
 
 # #         这道题最主要的地方在于要把到target 的k 距离的node 分为两个部分
 # #         第一部分：从target往下的子树里为k距离的node
@@ -83,6 +83,63 @@ class Solution:
 #             subtreeDistance(root.left, res, dist - 1)
 #             subtreeDistance(root.right, res, dist - 1)
     
+
+
+# 第三遍：
+        # 这道题一共分2个case
+        #     case1：从target 下面的substree找到dist ==k的node，也就是if node == target: 这里表述的case。
+        #     case2: case2.a 是target在当前node的分枝中。如果target在左分枝，那么我们就要到**右**分支找到k-left+1 的node，也是距离target是k的node
+        #            case2.b 如果target在右分枝，那么我们就要到**左**分支找到k-right+1 的node，也是距离target是k的node
+        #            case2.c 并且在case2 中还有一种情况就是，node自己本身就是到target 等于k距离的node，那么也要加进去
+        def dfs(node, res, k):
+            if not node:
+                return -1
+            
+            # case1:
+            if node == target:
+                find_nodes(node, res, k)
+                # 为什么这里return 1？ 是因为是一个bottom up 的过程。想象一下如果你站在target node的parent 位置，你应该得到
+                # 结果就是target在我（parent node）的分枝，距离我为1的分枝里。
+                return 1
+            
+            left = dfs(node.left, res, k)
+            right = dfs(node.right, res, k)
+            
+            # case2:
+            if left != -1:
+                # case2.c
+                if left == k:
+                    res.append(node.val)
+                # case2.a
+                find_nodes(node.right, res, k - left - 1)
+                return left + 1
+            if right != -1:
+                # case2.c
+                if right == k:
+                    res.append(node.val)
+                # case2.b
+                find_nodes(node.left, res, k - right - 1)
+                return right + 1
+            
+            return -1
+                    
+            
+        
+        
+        
+        def find_nodes(node, res, dist):
+            if not node:
+                return
+            if dist == 0:
+                res.append(node.val)
+
+            find_nodes(node.left, res, dist - 1)
+            find_nodes(node.right, res, dist - 1)
+        
+        
+        res = []
+        dfs(root, res, k)
+        return res
 
     def call_function(self) -> None:
         root = TreeNode(3)
